60f86559759e9cbca9736feb7278d724a9968300
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2020-09-20 01:43:40 -0500

    Updated file to render according to the route in url

--- Trabajo/client/src/App.tsx
+++ Trabajo/client/src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Landing } from './components/layout/Landing';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Landing/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Switch>
+    </BrowserRouter>
   );
   /*return (
     <div className="App">
