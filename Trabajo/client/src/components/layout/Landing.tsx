0d04ccbbc5ff5f1997767c8728a0f2a84aab47e5
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2020-09-21 15:05:12 -0500

    added footer and formatting

--- Trabajo/client/src/components/layout/Landing.tsx
+++ Trabajo/client/src/components/layout/Landing.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Button } from "reactstrap";
+import { FooterBar } from "./FooterBar";
 import "./layoutStyles.css";
 import { TopNavBar } from "./TopNavBar";
 
@@ -15,6 +16,9 @@ interface LandingState {
 
 }
 
+/* The landing page is the site home page - this is what displays when users visit
+    our website.  
+    */
 export class Landing extends React.Component<LandingProps, LandingState> {
 
     //constructor
@@ -26,7 +30,13 @@ export class Landing extends React.Component<LandingProps, LandingState> {
 
     render() {
         return (
-            <TopNavBar/>
+            <div>
+                <TopNavBar/>
+                <div className={"mainContentArea"}>
+                    <h1 className={"titleHeader"}>Trabajo</h1>
+                </div>
+                <FooterBar/>
+            </div>
         )
     }
 }
