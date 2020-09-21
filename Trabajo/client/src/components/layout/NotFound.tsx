51e7710eb8acd2a0023230b9bb45743e2c7f781d
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2020-09-21 15:06:36 -0500

    Created a 404 not found page that has a link back to the home page

---
+++ Trabajo/client/src/components/layout/NotFound.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { isWhiteSpaceLike } from "typescript";
+import { FooterBar } from "./FooterBar";
+import "./layoutStyles.css";
+import { TopNavBar } from "./TopNavBar";
+
+/* Interface for defining props for landing page
+ */
+interface NotFoundProps {
+    
+}
+
+/* Interface for defining state for landing page
+ */
+interface NotFoundState {
+
+}
+
+export class NotFound extends React.Component<NotFoundProps, NotFoundState> {
+    render() {
+        return (
+            <div>
+                <TopNavBar/>
+                <div className={"mainContentArea"}>
+                    <h1 className={"titleHeader"}>Error 404: Page Not Found</h1>
+                    <Link style={{color: "white", textAlign: "center"}} to="/">Return Home</Link>
+                </div>
+                <FooterBar/>
+            </div>
+        )
+    }
+}
+
