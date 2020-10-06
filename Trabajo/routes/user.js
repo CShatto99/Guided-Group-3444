5be500f4aa861bcc2fea286797229ed5fc713453
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-10-06 18:03:50 -0500

    Added register route

--- Trabajo/routes/user.js
+++ Trabajo/routes/user.js
@@ -1,57 +1,85 @@
 require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
-const { insertID, findUserByEmail } = require("../mongodb/user");
+const genAccessToken = require("../utils/genAccessToken");
+const genRefreshToken = require("../utils/genRefreshToken");
+const { insertUser, findUserByEmail } = require("../mongodb/user");
 
 router.post("/login", async (req, res) => {
-  const { _id, name } = req.body;
+  // pull body from request
+  const { email, password } = req.body;
 
-  const user = await insertID({ _id, name });
+  // check if any fields are empty
+  if (!email || !password)
+    return res.status(400).json({ msg: "Please enter an email and password" });
 
-  res.json({ user });
+  // check if user email exists
+  const lowercaseEmail = email.toLowerCase();
+  const findEmail = await findUserByEmail(lowercaseEmail);
+
+  if (findEmail.length === 0)
+    return res.status(400).json({ msg: "Invalid credentials" });
+
+  // check if password is correct
+  const match = await bcrypt.compare(password, findEmail[0].password);
+
+  if (!match) return res.status(400).json({ msg: "Invalid credentials" });
+
+  // generate access tokens
+  const accessToken = genAccessToken({ id: findEmail[0]._id });
+  const refreshToken = genRefreshToken({ id: findEmail[0]._id });
+
+  // store refresh token in httpOnly cookie
+  res.cookie("token", refreshToken, {
+    expires: new Date(Date.now() + 604800),
+    httpOnly: true,
+  });
+
+  // respond with access token
+  res.json({ accessToken });
 });
 
 router.post("/register", async (req, res) => {
-  const {
-    fullName,
-    email,
-    phone,
-    company,
-    companyCode,
-    address,
-    city,
-    state,
-    password,
-    confirmPassword,
-    companyImage,
-  } = req.body;
+  // pull body from request
+  const { fullName, email, password, confirmPassword } = req.body;
 
-  if (
-    !fullName ||
-    !email ||
-    !phone ||
-    !company ||
-    !companyCode ||
-    !address ||
-    !city ||
-    !state ||
-    !password ||
-    !confirmPassword ||
-    !companyImage
-  ) {
-    return res.status(400).json({ msg: "Please enter all required fields." });
-  }
+  // check if any fields are empty
+  if (!fullName || !email || !password || !confirmPassword)
+    return res
+      .status(400)
+      .json({ msg: "Please enter all required information." });
+
+  // check if user email already exists
+  const lowercaseEmail = email.toLowerCase();
+  const findEmail = await findUserByEmail(lowercaseEmail);
+  if (findEmail.length > 0)
+    return res.status(400).json({ msg: "Email already exists." });
 
   // Check if passwords match
-  if (password !== confirmPassword) {
+  if (password !== confirmPassword)
     return res.status(400).json({ msg: "Passwords do not match." });
-  }
 
-  // All form data exists and passwords match
+  // hash user password
+  const salt = await bcrypt.genSalt();
+  const hashedPass = await bcrypt.hash(password, salt);
 
-  res.send("hey");
+  // create and store new user
+  const newUser = { fullName, email, password: hashedPass };
+  const userInserted = await insertUser(newUser);
+
+  // generate access tokens
+  const accessToken = genAccessToken({ id: userInserted._id });
+  const refreshToken = genRefreshToken({ id: userInserted._id });
+
+  // store refresh token in httpOnly cookie
+  res.cookie("token", refreshToken, {
+    expires: new Date(Date.now() + 604800),
+    httpOnly: true,
+  });
+
+  // respond with access token
+  res.json({ accessToken });
 });
 
 module.exports = router;
