ede1cb59fcdb497892bdb29f1dedbc090a74803a
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-10-06 18:03:15 -0500

    commit

--- Trabajo/routes/user.js
+++ Trabajo/routes/user.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
 const genAccessToken = require("../utils/genAccessToken");
 const genRefreshToken = require("../utils/genRefreshToken");
