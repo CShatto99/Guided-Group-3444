becdc073506c04f7299849d6f3d231ca6c20a3d3
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2020-10-13 13:27:10 -0500

    Navbar now has logout button that works

--- Trabajo/client/src/components/layout/TopNavBar.tsx
+++ Trabajo/client/src/components/layout/TopNavBar.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navbar, Button, NavbarBrand, NavItem, Nav } from "reactstrap";
 import "./layoutStyles.css";
 import { RootState } from "../../store";
-import { UserState } from "../../store/user";
+import { logout, UserState } from "../../store/user";
 
 // Interface for defining props for landing page
 interface TopNavBarProps {}
@@ -11,7 +11,12 @@ interface TopNavBarProps {}
 /* This function is the top navigation bar shown on all web pages.  
     TODO: edit to show different buttons according to whether the user is logged in or not.*/
 export const TopNavBar: React.FC = () => {
+  const dispatch = useDispatch();
   const { isAuth } = useSelector<RootState, UserState>(state => state.user);
+
+  const userLogout = () => {
+    dispatch(logout());
+  }
 
   const guestLinks = (
     <>
@@ -41,8 +46,8 @@ export const TopNavBar: React.FC = () => {
         </Button>
       </NavItem>
       <NavItem>
-        <Button className={"navButton"} href="/" size="lg">
-          Other 2
+        <Button className={"navButton"} onClick={userLogout} size="lg">
+          Logout
         </Button>
       </NavItem>
     </>
