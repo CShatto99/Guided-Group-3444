cc83863a5f5918564cf8ff61ce6a76c1ce7adfad
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-10-16 18:56:15 -0500

    Added a route specifically for updateProfile.tsx

--- Trabajo/routes/profile.js
+++ Trabajo/routes/profile.js
@@ -9,6 +9,8 @@ const {
   findProfileByUser,
   updateProfile,
 } = require("../mongodb/profile");
+const { findUserByEmail } = require("../mongodb/user");
+const profile = require("../mongodb/profile");
 
 // @route GET /profile
 // @desc Get a user profile
@@ -41,7 +43,7 @@ router.post("/", authToken, async (req, res) => {
     rideDays,
     admin,
   } = req.body;
-/*
+  /*
   if (
     !name ||
     !email ||
@@ -64,7 +66,6 @@ router.post("/", authToken, async (req, res) => {
 
     // update profile if it already exists
     if (profileFound) {
-
       profileFound.name = profile.name;
       profileFound.email = profile.email;
       profileFound.company = profile.company;
@@ -88,4 +89,42 @@ router.post("/", authToken, async (req, res) => {
   }
 });
 
+// @route POST /profile/updateProfile
+// @desc Create/update a user profile
+// @access Private
+router.post("/updateProfile", authToken, async (req, res) => {
+  const { name, email, address, state, city, zip, rideDays } = req.body;
+
+  if (!name || !email || !address || !state || !city || !zip || !rideDays)
+    return res.status(400).json({ msg: "Please enter all required fields" });
+
+  try {
+    // If the email already exists, deny update
+    const profileExists = await findUserByEmail(email);
+    if (profileExists && req.user.ID.id !== profileExists._id.toString())
+      return res.status(400).json({ msg: "That email is taken" });
+
+    // If the profile already exists, update it
+    const profileFound = await findProfileByUser(req.user.ID.id);
+    if (profileFound) {
+      const newProfile = { ...profileFound, ...req.body };
+
+      await updateProfile(newProfile);
+
+      return res.json(newProfile);
+    }
+
+    // If the profile does not exist, create it
+    const profile = { ...req.body, ...{ userID: req.user.ID.id } };
+    const newProfile = await insertProfile(profile);
+
+    res.json(newProfile);
+  } catch (error) {
+    res.status(500).json({ msg: "Internal server error" });
+  }
+});
+
 module.exports = router;
+
+// need a route for adding a company page
+// need a route for adding riders page
