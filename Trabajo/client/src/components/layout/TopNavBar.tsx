8ef022e3e964cd2d44ba0270c8c9bdb2122f6cfb
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-10-16 21:21:14 -0500

    Made navbar responsive

--- Trabajo/client/src/components/layout/TopNavBar.tsx
+++ Trabajo/client/src/components/layout/TopNavBar.tsx
@@ -1,6 +1,14 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navbar, Button, NavbarBrand, NavItem, Nav } from "reactstrap";
+import {
+  Navbar,
+  Button,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Nav,
+} from "reactstrap";
 import "./layoutStyles.css";
 import { RootState } from "../../store";
 import { logout, UserState } from "../../store/user";
@@ -12,7 +20,10 @@ interface TopNavBarProps {}
     TODO: edit to show different buttons according to whether the user is logged in or not.*/
 export const TopNavBar: React.FC = () => {
   const dispatch = useDispatch();
-  const { isAuth, user} = useSelector<RootState, UserState>(state => state.user);
+  const { isAuth, user } = useSelector<RootState, UserState>(
+    state => state.user
+  );
+  const [isOpen, setIsOpen] = useState(false);
 
   const userLogout = () => {
     dispatch(logout());
@@ -21,12 +32,12 @@ export const TopNavBar: React.FC = () => {
   const guestLinks = (
     <>
       <NavItem>
-        <Button className={"navButton"} href="/login" size="lg">
+        <Button className={"navButton"} href="/login">
           Login
         </Button>
       </NavItem>
       <NavItem>
-        <Button className={"navButton"} href="/register" size="lg">
+        <Button className={"navButton"} href="/register">
           Register
         </Button>
       </NavItem>
@@ -55,9 +66,32 @@ export const TopNavBar: React.FC = () => {
 
   return (
     <div className={"topNavBar"}>
-      <Navbar>
-        <NavbarBrand href="/">{!isAuth ? "logo" : `Welcome ${user.fullName}!`}</NavbarBrand>
-        <Nav>{isAuth ? authLinks : guestLinks}</Nav>
+      <Navbar expand="md">
+        <NavbarBrand href="/" className="nav-logo">
+          <i className="fa fa-suitcase" aria-hidden="true"></i>
+          {isAuth && `Welcome ${user.fullName}!`}
+        </NavbarBrand>
+        {isOpen ? (
+          <i
+            aria-label="Toggle navigation"
+            className="fa fa-times fa-2x nav-icon"
+            aria-hidden="true"
+            onClick={() => setIsOpen(!isOpen)}
+          ></i>
+        ) : (
+          <i
+            aria-label="Toggle navigation"
+            className="fa fa-bars fa-2x nav-icon"
+            aria-hidden="true"
+            onClick={() => setIsOpen(!isOpen)}
+          ></i>
+        )}
+
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            {isAuth ? authLinks : guestLinks}
+          </Nav>
+        </Collapse>
       </Navbar>
     </div>
   );
