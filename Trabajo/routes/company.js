ab0dd4f92fe392d5807e14fae8d273a0442506c6
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-10-18 20:29:29 -0500

    Storing company names as lowercase

--- Trabajo/routes/company.js
+++ Trabajo/routes/company.js
@@ -5,15 +5,14 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 const authToken = require("../middleware/authToken");
 const {
-    insertCompany,
+  insertCompany,
   findAllCompanies,
-  findCompanyByName
+  findCompanyByName,
 } = require("../mongodb/company");
 
 // @route GET /company/all
 // @desc Gets all company names and images
 // @access Private
-
 router.get("/all", authToken, async (req, res) => {
   try {
     const companies = await findAllCompanies();
@@ -25,37 +24,55 @@ router.get("/all", authToken, async (req, res) => {
 
 // @route POST /company/create
 // @desc create a company
-// @access Public
+// @access Private
 router.post("/create", authToken, async (req, res) => {
-    // pull body from request
-  const { name, address, city, state, zip, code, confirmCode, image } = req.body;
+  // pull body from request
+  const {
+    name,
+    address,
+    city,
+    state,
+    zip,
+    code,
+    confirmCode,
+    image,
+  } = req.body;
 
   // check if any fields are empty
-  if (!name || !address || !city || !state || !zip || !code || !confirmCode )
+  if (!name || !address || !city || !state || !zip || !code || !confirmCode)
     return res.status(400).json({ msg: "Please enter all required fields" });
 
   // check if company already exists
-  const companyFound = await findCompanyByName(name);
-  if (companyFound) return res.status(400).json({ msg: "Company already exists." });
+  const lowercaseName = name.toLowerCase();
+  const companyFound = await findCompanyByName(lowercaseName);
+  if (companyFound)
+    return res.status(400).json({ msg: "Company already exists." });
 
   // Check if passwords match
   if (code !== confirmCode)
     return res.status(400).json({ msg: "Codes do not match." });
 
-    try {
-
+  try {
     // hash company password
     const salt = await bcrypt.genSalt();
     const hashedCode = await bcrypt.hash(code, salt);
 
     // create and store new user
-    const newCompany = { name, address, city, state, zip, hashedCode, image };
+    const newCompany = {
+      name: lowercaseName,
+      address,
+      city,
+      state,
+      zip,
+      hashedCode,
+      image,
+    };
     const company = await insertCompany(newCompany);
 
     return res.json(company);
-    } catch (error) {
-      return res.status(500).json({ msg: "Internal Server error" });
-    }
+  } catch (error) {
+    return res.status(500).json({ msg: "Internal Server error" });
+  }
 });
 
 module.exports = router;
