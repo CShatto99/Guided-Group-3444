90c617f7788c61709ce990ca740fc80c3fdb2b1a
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2020-10-21 12:59:14 -0500

    added comments for high level server file

--- Trabajo/server.js
+++ Trabajo/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
 
+//The following define settings to use for the server
 app.use(
   cors({
     origin: "http://localhost:5000",
@@ -17,10 +18,15 @@ app.use((_, res, next) => {
   next();
 });
 
+//the following are higher level endpoints, eg. http://localhost.com:5000/<higherlevel>/<lowerlevel>
+//the higher level routes are defined in the /routes folder
 app.use("/user", require("./routes/user"));
 app.use("/auth", require("./routes/auth"));
 app.use("/profile", require("./routes/profile"));
 app.use("/company", require("./routes/company"))
 
+//the port to use
 const port = process.env.PORT || 5000;
+
+//run the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
