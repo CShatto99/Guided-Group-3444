d8c6bff568600ce59a17ba98e26cd7757d6abb3a
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-10-27 16:57:15 -0500

    Added env var to client/.env

--- Trabajo/client/src/components/layout/UserHomeMap.jsx
+++ Trabajo/client/src/components/layout/UserHomeMap.jsx
@@ -1,21 +1,19 @@
-import {Map, GoogleApiWrapper} from 'google-maps-react';
-import React from 'react';
+import { Map, GoogleApiWrapper } from "google-maps-react";
+import React from "react";
 
 export class UserHomeMap extends React.Component {
-    
-
-    render () {
-        return (
-            <Map
-          google={this.props.google}
-          zoom={12}
-          style={{width: "100%", height: "100%"}}
-          initialCenter={{ lat: 33.254311, lng: -97.151867}}
-        />
-        )
-    }
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={12}
+        style={{ width: "100%", height: "100%" }}
+        initialCenter={{ lat: 33.254311, lng: -97.151867 }}
+      />
+    );
+  }
 }
- 
+
 export default GoogleApiWrapper({
-  apiKey: process.env.GOOGLE_MAPS_API_SECRET
-})(UserHomeMap)
+  apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_SECRET,
+})(UserHomeMap);
